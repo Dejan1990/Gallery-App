8d574882109b110fce1c24c11dcfae6c1785514a
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-05-25 18:27:15 +0200

    Upload Component part-2

--- resources/js/components/UploadComponent.vue
+++ resources/js/components/UploadComponent.vue
@@ -1,25 +1,41 @@
 <template>
-	<div>
-		<form method="post" enctype="multipart/form-data">
-			<div class="form-group files text-center" ref="fileform">
-				<input type="file" ref="file" multiple="multiple">
-				<span id="val"></span>
-				<a class="btn btn-secondary" @click.prevent="submitFiles()">Upload</a>
-			</div>
-		</form>
-    </div>
+<div>
+    <form method="post" enctype="multipart/form-data">
+        <div class="form-group files text-center" ref="fileform">
+            <input type="file" ref="file" multiple="multiple">
+            <span id="val"></span>
+            <a class="btn btn-secondary" @click.prevent="submitFiles()">Upload</a>
+        </div>
+    </form>
+</div>
 </template>
+
 <script>
 export default {
     data() {
         return {
-            
+            album_id: 1
         }
     },
     methods: {
         submitFiles() {
-            alert('OK')
-        }
+            const config = {
+                headers: {
+                    "content-type": "multipart/form-data"
+                }
+            }
+            let formData = new FormData();
+            for (var i = 0; i < this.$refs.file.files.length; i++) { //this.$refs.file.files.length gives total length of images
+                let file = this.$refs.file.files[i];
+                formData.append('files[' + i + ']', file);
+                formData.append('album_id', this.album_id)
+            }
+            this.uploading = true;
+            this.$refs.file.value = ''
+            axios.post('/uploadImages', formData, config).then((response) => {
+
+            })
+        },
     },
 }
 </script>
